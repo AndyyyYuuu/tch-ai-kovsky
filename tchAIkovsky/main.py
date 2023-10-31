25812d76348995fc078cd8a88939d8ce37a35fd5
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-10-31 12:09:18 -0700

    Add a get notes function

--- tchAIkovsky/main.py
+++ tchAIkovsky/main.py
@@ -1,2 +1,24 @@
 import torch, music21
 device = torch.device("cpu")
+
+def get_notes_list_from_stream(midi_stream):
+    note_filter = music21.stream.filters.ClassFilter('Note')
+    stream_notes = []
+    for note in midi_stream.recurse().addFilter(note_filter):
+        note_dict = {
+            'nameWithOctave': note.nameWithOctave,
+            'fullName': note.fullName,
+            'word': '{}_{}_{}'.format(note.pitch.name, str(note.pitch.octave), str(note.duration.type)).lower(),
+            'pitch': {
+                'name': note.pitch.name,
+                'microtone': str(note.pitch.microtone),
+                'octave': str(note.pitch.octave),
+                'step': str(note.pitch.step)
+            },
+            'duration':{
+                'type': str(note.duration.type)
+            }
+        }
+        stream_notes.append(note_dict)
+    return stream_notes
+
